cb4783ca2df380541ced9741ac3651145bba2ae1
Kedaar Chakankar <kchakankar@upatoday.com>
2020-09-29 00:16:51 +0000

    for loop name thing

--- Main.java
+++ Main.java
@@ -1,6 +1,11 @@
 class Main {
   public static void main(String[] args) {
+		String x = "Kedaar";
     for (int i = 0; i < 10; i++)
-			System.out.println("guacamole nigga penis " + (i+1));
+		{
+			for(int j = 0; j < i; j++)
+				System.out.print(" ");
+			System.out.println(x.substring(i, i+1));
+		}
   }
 }
